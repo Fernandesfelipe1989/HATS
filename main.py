1bd627eaae76d8ca7b5f8021358f1b695805119b
Felipe O Fernandes <42359721+Fernandesfelipe1989@users.noreply.github.com>
2020-09-24 15:33:38 -0300

    Criação da função input

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 def converter_to_base(S, b, k):
     number_in_new_base = []
-    # TOD: Avisar o usuário quando a base é insuficiente?
+    # TOD0: Avisar o usuário quando a base é insuficiente?
     for i in (S):
         div = i
-        flag_converter = True
-        # print(i)
         converter =''
         while len(converter) < k:
             if div != 0:
@@ -17,6 +15,46 @@ def converter_to_base(S, b, k):
 
     return number_in_new_base
 
-print(converter_to_base([9, 30], 3, 2))
 
+def base_sum(number):
+    for i in number:
+        for a in i:
+            pass
+        pass
+    pass
+
+
+def main():
+    while True:
+        try:
+            n = int(input("Insert the space number: "))
+            break
+        except:
+            print('Invalid input!')
+
+    while True:
+        try:
+            k = int(input("Insert the number of pile of stones: "))
+            break
+        except:
+            print('Invalid input!')
+    S = []
+    for i in range(n):
+        s = int(input(f'Insert the number {i + 1}: '))
+        try:
+            S.append(s)
+        except:
+            print('Invalid input!')
+
+    while True:
+        try:
+            b = int(input("Insert base: "))
+            break
+        except:
+            print('Invalid input!')
+
+    print(converter_to_base(S, b, k))
+
+if __name__ == "__main__":
+    main()
 
